62dd4296505ecbeade113c1992da0543437c0554
John Cena <sadhineniyaswanthkumar@gmail.com>
2026-07-03 20:45:45 +0530

    Added Vercel Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import { Geist, Geist_Mono } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 const geistSans = Geist({ variable: '--font-geist-sans', subsets: ['latin'] })
@@ -18,7 +19,10 @@ export default function RootLayout({ children }) {
           href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
         />
       </head>
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
